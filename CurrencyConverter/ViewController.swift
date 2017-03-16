41c71f339884fc5fe49ff04ad3aeb105222f7130
Stephen Guined <sguined@gmail.com>
2017-03-16 00:07:20 -0700

    CurrencyConverter commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Stephen Guined on 3/9/17.
+//  Copyright © 2017 CareerFoundry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //
+    @IBOutlet weak var poundLabel: UILabel!
+    @IBOutlet weak var yenLabel: UILabel!
+    @IBOutlet weak var euroLabel: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+   
+    
+    let poundRate = 0.69
+    let yenRate = 113.94
+    let euroRate = 0.89
+    var dollarAmount = 0.0
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        inputTextField.delegate = self
+    }
+
+
+    @IBAction func convertCurrency(_ sender: UIButton) {
+        if let amount = Double(inputTextField.text!) {
+            dollarAmount = amount
+        }
+        poundLabel.text = "\(dollarAmount * poundRate)"
+        yenLabel.text = "\(dollarAmount * yenRate)"
+        euroLabel.text = "\(dollarAmount * euroRate)"
+        dollarAmount = 0.0
+    }
+    
+    // Clear Button
+    @IBAction func clearButton(_ sender: Any) {
+        inputTextField.text = ""
+        yenLabel.text = "0.00"
+        euroLabel.text = "0.00"
+        poundLabel.text = "0.00"
+        return
+    }
+    
+
+    // Called when 'return' key is pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Called when user taps outside the text field
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
